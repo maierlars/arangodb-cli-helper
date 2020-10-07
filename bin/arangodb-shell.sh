392e4aafb1610d5393d8acbe7d9ae54d6a78deb6
Tobias Gödderz <tobias@arangodb.com>
2020-10-07 16:27:51 +0200

    Added usage information to adb

--- bin/arangodb-shell.sh
+++ bin/arangodb-shell.sh
@@ -1,10 +1,34 @@
 #!/bin/bash
 
+
+function usage {
+    cat <<USAGE
+Usage: $0 [-h|--help] <command> [<args>]
+  -h|--help           show this usage information
+Commands:
+  jenkins             start a jenkins pr matrix on the current branch
+USAGE
+}
+
+if [ $# -eq 0 ]
+then
+    usage
+    exit 1
+fi
+
+
 case $1 in
-	jenkins)
-		arangodb-jenkins-run-pr-post.py
-		;;
-	*)
-		echo "Unknown command $1"
-		;;
+        jenkins)
+                arangodb-jenkins-run-pr-post.py
+                ;;
+        -h|--help)
+                usage
+                exit 0
+                ;;
+        *)
+                echo "Unknown command or option: $1"
+                echo ""
+                usage
+                exit 1
+                ;;
 esac
